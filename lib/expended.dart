d2acd8f67d9159b13b8f14e3ec2b52e58e44bfbd
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-05-06 14:34:07 +0600

    flutter expanded flex done

--- lib/expended.dart
+++ lib/expended.dart
@@ -1,6 +1,63 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-void main(){
-  return runApp(StartExpandedWidget());
+void main() {
+  return runApp(const StartExpandedWidget());
 }
 
+class StartExpandedWidget extends StatelessWidget {
+  const StartExpandedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ExpendedWidget",
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: const HomePageForExpendedWidget(),
+    );
+  }
+}
+
+class HomePageForExpendedWidget extends StatelessWidget {
+  const HomePageForExpendedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expended Navbar"),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+              child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+          )),
+          Expanded(
+            flex: 2,
+              child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.deepOrange,
+          )),
+          Expanded(
+              child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.green,
+          )),
+          Expanded(
+              child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.deepPurpleAccent,
+          ))
+        ],
+      ),
+    );
+  }
+}
